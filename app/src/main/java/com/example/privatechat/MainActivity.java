d159d50aa06303a98629c2bfe13e03786452c508
w.hasnat94@gmail.com <git94w.hasnat>
2020-06-16 04:38:53 -0700

    Tabs Added and LoginActivity Added

--- app/src/main/java/com/example/privatechat/MainActivity.java
+++ app/src/main/java/com/example/privatechat/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.privatechat;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,20 +8,43 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewPager myViewPage;
+    private Toolbar myToolbar;
+    private ViewPager myViewPager;
     private TabLayout myTabLayout;
+    private TabAccessorAdapter myTabAccessorAdapter;
+    private FirebaseUser currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myViewPage = (ViewPager) findViewById(R.id.main_tabs_pager);
-        Toolbar mytoolbar = (Toolbar) findViewById(R.id.main_page_bar);
-        setSupportActionBar(mytoolbar);
+
+        myToolbar = (Toolbar) findViewById(R.id.main_page_bar);
+        setSupportActionBar(myToolbar);
         getSupportActionBar().setTitle("Private Chat");
+
+        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        myTabAccessorAdapter = new TabAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabAccessorAdapter);
+
+        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(currentUser == null){
+            sendUserToLoginActivity();
+        }
+    }
+
+    private void sendUserToLoginActivity() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
